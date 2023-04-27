8ee96cfc110af147a7e8b665baa7e788437f735f
AkumuJey <josephowino7@gmail.com>
2023-04-27 18:27:03 +0300

    Pinia is hard..but I keep the fight

--- src/App.vue
+++ src/App.vue
@@ -5,18 +5,70 @@ import ProductCard from "@/components/ProductCard.vue";
 
 import { useProductStore } from "./stores/ProductStore";
 import { useCartStore } from "./stores/CartStore";
+import { ref, toRef, reactive } from "vue";
 
 const productStore = useProductStore()
 const cartStore = useCartStore()
 
 productStore.fill()
 
+const future = reactive([])
+const history = reactive([])
+
+const doingHistory = ref(false)
+
+const unDo = () => {
+  if(history.length === 1) return
+  doingHistory.value = true
+  future.push(history.pop())
+  cartStore.$state = JSON.parse(history.at(-1))
+  doingHistory.value = false
+}
+
+const reDo = () => {
+  const latestState = future.pop()
+  if(!latestState) return
+  doingHistory.value = true
+  history.push(latestState)
+  cartStore.$state = JSON.parse(latestState)
+  doingHistory.value = false
+}
+
+
+history.push(JSON.stringify(cartStore.$state))
+cartStore.$subscribe((mutation, state) => {
+  if(!doingHistory.value) {
+    history.push(JSON.stringify(state))
+    future.splice(0, future.length)
+  }
+})
+
+cartStore.$onAction(({
+  name,
+  store,
+  args,
+  after,
+  onError
+}) => {
+  if(name === 'addItems') {
+    after(() => {
+      console.log(args[0]);
+    }),
+    onError((error) => {
+      console.log(error.message);
+    })
+  }
+})
 
 </script>
 
 <template>
   <div class="container">
     <TheHeader />
+    <div class="mb-5 flex justify-center">
+      <AppButton @click="unDo">Undo</AppButton>
+      <AppButton class="ml-2" @click="reDo">Re-do</AppButton>
+    </div>
     <ul class="sm:flex flex-wrap lg:flex-nowrap gap-5">
       <ProductCard
         v-for="product in productStore.products"

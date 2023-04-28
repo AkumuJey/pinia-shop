768da159a7c83ea15f5169a35ace4b1468755cb4
AkumuJey <josephowino7@gmail.com>
2023-04-28 22:43:18 +0300

    Vue Use project done

--- src/App.vue
+++ src/App.vue
@@ -5,43 +5,11 @@ import ProductCard from "@/components/ProductCard.vue";
 
 import { useProductStore } from "./stores/ProductStore";
 import { useCartStore } from "./stores/CartStore";
-import { ref, toRef, reactive } from "vue";
-
-const productStore = useProductStore()
 const cartStore = useCartStore()
-
+const productStore = useProductStore()
 productStore.fill()
 
-const future = reactive([])
-const history = reactive([])
 
-const doingHistory = ref(false)
-
-const unDo = () => {
-  if(history.length === 1) return
-  doingHistory.value = true
-  future.push(history.pop())
-  cartStore.$state = JSON.parse(history.at(-1))
-  doingHistory.value = false
-}
-
-const reDo = () => {
-  const latestState = future.pop()
-  if(!latestState) return
-  doingHistory.value = true
-  history.push(latestState)
-  cartStore.$state = JSON.parse(latestState)
-  doingHistory.value = false
-}
-
-
-history.push(JSON.stringify(cartStore.$state))
-cartStore.$subscribe((mutation, state) => {
-  if(!doingHistory.value) {
-    history.push(JSON.stringify(state))
-    future.splice(0, future.length)
-  }
-})
 
 cartStore.$onAction(({
   name,
@@ -66,8 +34,8 @@ cartStore.$onAction(({
   <div class="container">
     <TheHeader />
     <div class="mb-5 flex justify-center">
-      <AppButton @click="unDo">Undo</AppButton>
-      <AppButton class="ml-2" @click="reDo">Re-do</AppButton>
+      <AppButton @click="cartStore.unDo">Undo</AppButton>
+      <AppButton class="ml-2" @click="cartStore.reDo">Re-do</AppButton>
     </div>
     <ul class="sm:flex flex-wrap lg:flex-nowrap gap-5">
       <ProductCard
--- src/stores/CartStore.js
+++ src/stores/CartStore.js
@@ -2,10 +2,13 @@ import { defineStore, acceptHMRUpdate } from "pinia";
 import { groupBy } from "lodash";
 import { useAuthUserStore } from "./AuthUserStore";
 
+import { useLocalStorage } from "@vueuse/core";
+
 export const useCartStore = defineStore('CartStore', {
+    historyEnabled: true,
     state: () => {
         return {
-            items: []
+            items: useLocalStorage('CartStore:items', [])
         }
     },
     getters: {
